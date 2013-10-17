764e5e62e9fc0df991de0a6ac3d8e594357a880e
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2013-10-17 20:46:29 +1100

    first commit

---
+++ manifests/jekyll.pp
@@ -0,0 +1,40 @@
+# == Class: jekyll::jekyll
+#
+# Goes into manifests/jekyll.pp
+
+class jekyll::jekyll {
+	# resources
+	package { "rubygems":
+		ensure => present,
+	}
+	package { "ruby1.9.1-dev":
+		ensure => present,
+	}
+	package { "ruby1.8-dev":
+		ensure => present,
+	}
+	package {'jekyll':
+		ensure     => latest,
+		provider   => 'gem',
+		require    => Package['rubygems', 'ruby1.8-dev', 'ruby1.9.1-dev', 'build-essential'],
+	}
+	package { "activesupport":
+		ensure   => '3.1.0',
+		provider => 'gem',
+		require  => Package['jekyll'],
+	}
+	package { "kramdown":
+		ensure   => present,
+		provider => 'gem',
+		require  => Package['jekyll'],
+	}
+	package { "i18n":
+		ensure   =>  present,
+		provider => 'gem',
+	}
+	package { "thor":
+		ensure   => present,
+		provider => 'gem',
+		require  => Package['activesupport', 'i18n']
+	}
+}
